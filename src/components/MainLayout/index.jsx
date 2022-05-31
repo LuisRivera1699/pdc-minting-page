3264b4667590e1a051c667c469d74b8e7a5d942b
Luis Rivera <luisriveradiaz99@gmail.com>
2022-05-31 17:24:21 -0500

    update dogs

--- src/components/MainLayout/index.jsx
+++ src/components/MainLayout/index.jsx
@@ -1,33 +1,9 @@
 import "./index.css";
-import dog1 from "../../assets/dog1.webp";
-import dog2 from "../../assets/dog2.webp";
-import dog3 from "../../assets/dog3.webp";
-import dog4 from "../../assets/dog4.webp";
-import dog5 from "../../assets/dog5.webp";
-import dog6 from "../../assets/dog6.webp";
+import dogs from "../../assets/dogs.gif";
 import Header from "../Header";
 import TabSelector from "../TabSelector";
-import { useEffect, useState } from "react";
 
 const MainLayout = (props) => {
-    const dogList = [dog1, dog2, dog3, dog4, dog5, dog6];
-    const [dogIdx, setDogIdx] = useState(0);
-
-    useEffect(() => {
-        const interval = setInterval(
-            () => {
-                if (dogIdx === dogList.length-1) {
-                    setDogIdx(0);
-                } else {
-                    let curIdx = dogIdx;
-                    setDogIdx(curIdx+1);
-                }
-            }, 2000
-        );
-
-        return () => clearInterval(interval);
-    });
-
     return (
         <div className="page__container">
             <Header/>
@@ -35,7 +11,7 @@ const MainLayout = (props) => {
                 <div className="__content">
                     <TabSelector/>
                     <div className="image__container">
-                        <img src={dogList[dogIdx]} alt=""/>
+                        <img src={dogs} alt=""/>
                     </div>
                     <div className="child__content">
                         {props.children}
