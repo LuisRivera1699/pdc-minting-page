2030079eed5685089cc1e4460fb5175521e5ce08
Luis Rivera <luisriveradiaz99@gmail.com>
2022-06-29 08:17:38 -0500

    add whitepaper to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ const Header = (props) => {
         <header>
             <img src={logo} alt=""/>
             <span className="header__option"><a href="https://peruviandogclub.io" target="_blank" rel="noreferrer">Website</a></span>
-            {/* <span className="header__option"><a href="https://peruviandogclub.io" target="_blank" rel="noreferrer">Whitepaper</a></span> */}
+            <span className="header__option"><a href="https://bald-syzygy-1a8.notion.site/WHITEPAPER-ESPA-OL-7e87005c96d64d05b0fd33920fe2dabe" target="_blank" rel="noreferrer">Whitepaper</a></span>
         </header>
     );
 }
